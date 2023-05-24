5790a252963f13cf45f15ca212945516e1cc8ab0
BlackE <elizabeth.ramescamilla@gmail.com>
2023-05-23 23:42:28 -0600

    chore(index): full front

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
 			<div class="container">
 				<div class="logoContainer"><a href="/"><img src="assets/OLICER-logo.png"></a></div>
 				<div class="menuContainer">
+					<input type="checkbox" id="menu"/>
+					<label for="menu"></label>
 					<ul class="menu">
 						<li><a href="/">Inicio</a></li>
 						<li><a href="/nosotros">Nosotros</a></li>
@@ -21,34 +23,122 @@
 		</header>
 		<main>
 			<section class="slideshow-container-main">
-				<section class="slideshow-container">
-					<section class="mySlides fade">
+				<div class="slideshow-container">
+					<div class="mySlides fade">
 					    <div class="container-text">
-					    	<div class="numbertext">Puentes vehiculares 1 / 3</div>
-					    	<div class="text"><a href="/proyectos#puentesvehiculares" target="_blank">Ver Rubro</a></div>
+					    	<div class="titleContainer"><h1>Puentes vehiculares</h1></div>
+					    	<div class="buttonContainer"><a class="button-rubro" href="/proyectos#puentes-vehiculares" target="_blank">Ver Rubro</a></div>
 					    </div>
-					</section>
+					</div>
 
-					<section class="mySlides fade">
+					<div class="mySlides fade">
 					     <div class="container-text">
-					    	<div class="numbertext">Puentes vehiculares 2 / 3</div>
-					    	<div class="text"><a href="/proyectos#puentesvehiculares" target="_blank">Ver Rubro</a></div>
+					    	<div class="titleContainer"><h1>Puentes peatonales</h1></div>
+					    	<div class="buttonContainer"><a class="button-rubro" href="/proyectos#puentes-peatonales" target="_blank">Ver Rubro</a></div>
 					    </div>
-					</section>
+					</div>
 
-					<section class="mySlides fade">
+					<div class="mySlides fade">
 					     <div class="container-text">
-					    	<div class="numbertext">Puentes vehiculares 3 / 3</div>
-					    	<div class="text"><a href="/proyectos#puentesvehiculares" target="_blank">Ver Rubro</a></div>
+					    	<div class="titleContainer"><h1>Concreto hidráulico</h1></div>
+					    	<div class="buttonContainer"><a class="button-rubro" href="/proyectos#concreto-hidraulico" target="_blank">Ver Rubro</a></div>
 					    </div>
-					</section>
+					</div>
 
+					<div class="mySlides fade">
+					     <div class="container-text">
+					    	<div class="titleContainer"><h1>Concreto asfáltico</h1></div>
+					    	<div class="buttonContainer"><a class="button-rubro" href="/proyectos#concreto-asfaltico" target="_blank">Ver Rubro</a></div>
+					    </div>
+					</div>
+
+					
+					<div class="mySlides fade">
+					     <div class="container-text">
+					    	<div class="titleContainer"><h1>Proyecto hidráulico</h1></div>
+					    	<div class="buttonContainer"><a class="button-rubro" href="/proyectos#proyecto-hidraulico" target="_blank">Ver Rubro</a></div>
+					    </div>
+					</div>
+					
+					<div class="mySlides fade">
+					     <div class="container-text">
+					    	<div class="titleContainer"><h1>Edificación</h1></div>
+					    	<div class="buttonContainer"><a class="button-rubro" href="/proyectos#edificacion" target="_blank">Ver Rubro</a></div>
+					    </div>
+					</div>
 					  <!-- Next and previous buttons -->
 					<div class="buttons">
 					  <span class="prev">&#8592;</span>
 					  <span class="next">&#8594;</span>
 					</div>
-				</section>
+				</div>
+			</section>
+
+
+			<section class="textSection">
+				<div class="container">
+					<div class="titleContainer">
+						<h2>EXPERTOS EN<br><b>INFRAESTRUCTURA</b><br>URBANA</h2>
+					</div>
+					<div class="descriptionContainer">
+						<p>Si buscas una empresa de construcción comprometida con la excelencia y la calidad en todos sus proyectos, <b>¡has llegado al lugar indicado!</b></p>
+					</div>
+				</div>
+				<div class="ctaSection">
+					<a href="contacto">¡ME INTERESA!</a>
+				</div>
+			</section>
+			<section class="nosotrosSection">
+				<div class="container">
+					<div class="titleContainer">
+						<h3>NOSOTROS</h3>
+						<p>DEDICADOS A REALIZAR PROYECTOS QUE MARCAN LA DIFERENCIA</p>
+					</div>
+					<div class="descriptionContainer">
+						<p>OLICER, fundada el 10 de <b>julio</b> de <b>2002</b>, somos una empresa líder en el mercado de la construcción, con amplia experiencia en el diseño y ejecución de ingeniería de <b>alta complejidad</b>.</p>
+						<a class="conocer" href="/nosotros">CONOCER MÁS</a>
+					</div>
+				</div>
+			</section>
+			
+			<section class="serviciosSection">
+				<div class="container">
+					<div class="textContainer">
+						<h3>SERVICIOS</h3>
+						<p>Nos enfocamos en brindar un servicio de <b>calidad, personalizado y eficiente</b>, asegurando la satisfacción total de nuestros clientes.</p>
+						<p>Contamos con un equipo <b>altamente capacitado</b> de ingenieros, arquitectos y especialistas en diversas áreas de la construcción, que trabajan en conjunto para lograr resultados sobresalientes.</p>
+						<a class="conocer" href="/nosotros">CONOCER MÁS</a>
+					</div>
+				</div>
+			</section>
+			
+			<section class="serviciosGridSection">
+				<div class="container">
+					<div class="serviceCard">
+						<img src="assets/inicio-estudios-thumbnail.png"/>
+						<p>Estudios preliminares</p>
+					</div>
+
+					<div class="serviceCard">
+						<img src="assets/inicio-proyectos-thumbnail.png"/>
+						<p>Proyectos Ejecutivos</p>
+					</div>
+
+					<div class="serviceCard">
+						<img src="assets/inicio-obra-thumbnail.png"/>
+						<p>Obra Civil</p>
+					</div>
+
+					<div class="serviceCard">
+						<img src="assets/inicio-infra-vial-thumbnail.png"/>
+						<p>Infraestructura víal y urbana</p>
+					</div>
+
+					<div class="serviceCard">
+						<img src="assets/inicio-infra-hidra-thumbnail.png"/>
+						<p>Infraestructura hidráulica y sanitaria</p>
+					</div>
+				</div>
 			</section>
 		</main>
 		<?php include("footer.php");?>
